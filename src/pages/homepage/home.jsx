1870ce4f213718887253f5a53e389349c3eb751b
KOR-GEON-WOO <nextstep9902@naver.com>
2024-06-26 14:07:00 +0900

    feat: 네비 바 추가

--- src/pages/homepage/home.jsx
+++ src/pages/homepage/home.jsx
@@ -1,13 +1,17 @@
-import React from 'react' // eslint-disable-line no-unused-vars
+import React from "react"; // eslint-disable-line no-unused-vars
 
-const home = () => {
+const Home = () => {
   return (
     <div>
       <nav>
         <p>메뉴</p>
       </nav>
+      {/* 여기에 footer를 추가합니다 */}
+      <footer>
+        <p>© 2024 NongBu. All rights reserved.</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default home
+export default Home;
---
+++ src/pages/navbar/navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react"; // eslint-disable-line no-unused-vars
+
+const Home = () => {
+  return (
+    <div className="nav">
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">NongBu</a>
+          </li>
+          <li>
+            <a href="#land_list">토지 리스트</a>
+          </li>
+          <li>
+            <a href="#service">서비스 소개</a>
+          </li>
+          <li>
+            <a href="#faq">FAQ</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
