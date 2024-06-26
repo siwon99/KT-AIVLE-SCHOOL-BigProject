ae9db1872741c1f6f319ecf2db099302852097c0
siwon99 <kimsiwon0707@gmail.com>
2024-06-26 13:26:32 +0900

    feat: 페이지 나누기

---
+++ src/pages/detailpage/detail.jsx
@@ -0,0 +1,11 @@
+import React from 'react' // eslint-disable-line no-unused-vars
+
+const detail = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default detail
---
+++ src/pages/loginpage/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react' // eslint-disable-line no-unused-vars
+
+const login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default login
