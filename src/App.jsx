5c5e3044898c592037946767e9c89a04183e9f98
hyemin <hm2743@naver.com>
2024-06-26 15:29:06 +0900

    앱 홈 호출 선언

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import './App.css'
-import WhomeIcon from "./assets/WhomeIcon.svg"
 import Home from "./pages/homepage/home"
 
 function App() {
 
   return (
     <>
-      <nav className='navbar'>
-        <ul>
-          <img className='homeicon' src={WhomeIcon} alt="WhomeIcon" />
-          <li><a href='#'>FAQ</a></li>
-          <li><a href='#'>서비스 소개</a></li>
-        </ul>
-      </nav>
-      <div className='main'>
-      </div>
+      <Home/>
     </>
   )
 }
