43545dc42d9aa068a832fb2af23219f5dc399a6b
siwon99 <kimsiwon0707@gmail.com>
2024-06-26 09:42:59 +0900

    feat: 메인 페이지 메뉴

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 
+import WhomeIcon from "./assets/WhomeIcon.svg"
+
+
 function App() {
 
   return (
     <>
-      <div>
-        <p>
-          메인 페이지 입니다.
-        </p>
+      <nav className='navbar'>
+        <ul>
+          <img className='homeicon' src={WhomeIcon} alt="WhomeIcon" />
+          <li><a href='#'>FAQ</a></li>
+          <li><a href='#'>서비스 소개</a></li>
+        </ul>
+      </nav>
+      <div className='main'>
       </div>
     </>
   )
