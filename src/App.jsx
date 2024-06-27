71a0b024e83b533f6a53c4470c5475659301df0f
KOR-GEON-WOO <nextstep9902@naver.com>
2024-06-27 11:46:06 +0900

    listpage.jsx, app.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'; // eslint-disable-line no-unused-vars
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/homepage/home';
 import Login from './pages/loginpage/login';
+import ListPage from './pages/listpage/listpage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/list" element={<ListPage />} />
       </Routes>
     </Router>
   );
--- src/pages/listpage/listpage.jsx
+++ src/pages/listpage/listpage.jsx
@@ -1,11 +1,34 @@
-import React from 'react' // eslint-disable-line no-unused-vars
+import React from 'react'; // eslint-disable-line no-unused-vars
+import Navbar from '../navbar/navbar'; // navbar.jsx 파일 임포트
 
-const listpage = () => {
+const ListPage = () => {
   return (
     <div>
-      
+      <Navbar />
+      <main>
+        <h1>토지 리스트</h1>
+        <div>
+          <ul>
+            <li>
+              <span>No.1</span>
+              <span>[대전] 탄방동 100cm^2 농지</span>
+              <button>선택</button>
+            </li>
+            <li>
+              <span>No.2</span>
+              <span>[대전] 유성구 1000cm^2 농지</span>
+              <button>선택</button>
+            </li>
+            <li>
+              <span>No.3</span>
+              <span>[천안] 두정동 500cm^2 농지</span>
+              <button>선택</button>
+            </li>
+          </ul>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default listpage
+export default ListPage;
