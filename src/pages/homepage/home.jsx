f8331c4ebb9c5e2a681d6d60be93dd8079d4270a
hyemin <hm2743@naver.com>
2024-06-27 20:20:59 +0900

    feat: 홈 영역 배치 재설정(2)

--- src/pages/homepage/home.jsx
+++ src/pages/homepage/home.jsx
@@ -13,11 +13,11 @@ const MainText = () => (
 // 메인 페이지 유튜브
 const Youtube = () => (
   <section className="youtube-container">
-    <h3>공식 영상</h3>
+    {/* <h3>공식 영상</h3> */}
     <div className="youtube">
       <iframe
         width="560"
-        height="315"
+        height="350"
         src="https://www.youtube.com/embed/VIDEO_ID"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
