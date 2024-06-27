6d5a414cadf443e068a09d120713084acf4d59a0
byeonggwan <choosymail@gmail.com>
2024-06-27 17:25:55 +0900

    feat: 토큰 여부에 따른 아이콘 교체

--- src/pages/navbar/navbar.jsx
+++ src/pages/navbar/navbar.jsx
@@ -1,11 +1,19 @@
-import React from "react"; // eslint-disable-line no-unused-vars
+import React, {useEffect, useState} from "react"; // eslint-disable-line no-unused-vars
 import "./navbar.css";
 import WhomeIcon from "../../assets/WhomeIcon.svg";
 import WlogIn from "../../assets/WlogIn.svg";
 import WsignUp from "../../assets/WsignUp.svg";
+import WlogOut from "../../assets/WlogOut.svg";
 
 // Navbar 컴포넌트 정의
 const Navbar = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setToken(token);
+  }, []);
+
   return (
     <div className="navbar">
       <nav>
@@ -20,12 +28,21 @@ const Navbar = () => {
           <li className="category"><a href="/service">서비스 소개</a></li>
           <li className="category"><a href="/faq">FAQ</a></li>
           <li className="auth-icons">
-            <a href="/login">
-              <img src={WlogIn} alt="Log In" className="login-icon" />
-            </a>
-            <a href="/signup">
-              <img src={WsignUp} alt="SignUp" className="signup-icon" />
-            </a>
+            {!token ? (
+                <div>
+                  <a href="/login">
+                    <img src={WlogIn} alt="Log In" className="login-icon" />
+                  </a>
+                  <a href="/signup">
+                    <img src={WsignUp} alt="SignUp" className="signup-icon" />
+                  </a>
+                </div>
+              ) : (
+                <a href="/logout">
+                  <img src={WlogOut} alt="Log Out" className="logout-icon" />
+                </a>
+              )
+            }
           </li>
         </ul>
       </nav>
