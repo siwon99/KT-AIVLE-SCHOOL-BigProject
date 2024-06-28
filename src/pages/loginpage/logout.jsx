b59adc960326cd101ec66019d5b232f904b06000
byeonggwan <choosymail@gmail.com>
2024-06-28 13:58:13 +0900

    fix: 로그아웃 기능 개선

--- src/pages/loginpage/logout.jsx
+++ src/pages/loginpage/logout.jsx
@@ -7,15 +7,14 @@ const Logout = () => {
   useEffect(() => {
     //사용자가 로그인할 때 저장된 토큰
     const token = localStorage.getItem('token');
-
+    
     if (token) {
       fetch('http://3.39.228.42/users/logout/', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({ token }), 
+          'Authorization': `Token  ${token}`,
+        }, 
       })
       .then(response => {
         if (response.ok) {
@@ -25,16 +24,16 @@ const Logout = () => {
         }
       })
       .then(data => {
-        if (data.success) {
+        if (data.detail) {
           localStorage.removeItem('token');
-          navigate('/login');
-          alert('로그아웃에 성공했습니다.');
-        } else {
-          alert('로그아웃에 실패했습니다. 다시 시도해주세요.');
+          navigate('/');
+        }
+        else {
+          throw new Error();
         }
       })
       .catch(error => {
-        alert('오류가 발생했습니다. 다시 시도해주세요.');
+        alert('오류가 발생했습니다.');
       });
     } else {
       navigate('/login');
