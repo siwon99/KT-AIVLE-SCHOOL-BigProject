2aa8001ffd1c54f2629c487edc5fcee13d793282
siwon99 <kimsiwon0707@gmail.com>
2024-07-01 13:28:39 +0900

    feat: 엔터키 실행시 로그인

--- src/pages/loginpage/UserInput.jsx
+++ src/pages/loginpage/UserInput.jsx
@@ -1,7 +1,14 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 
-const UserInput = ({ type, placeholder, value, name, onChange }) => {
+const UserInput = ({ type, placeholder, value, name, onChange, onEnterPress }) => {
+  //엔터키 누르면 로그인
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      onEnterPress();
+    }
+  };
+
   return (
     <input
       type={type}
@@ -10,6 +17,7 @@ const UserInput = ({ type, placeholder, value, name, onChange }) => {
       name={name}
       onChange={onChange}
       className="user-input"
+      onKeyPress={handleKeyPress}
     />
   );
 };
@@ -20,6 +28,7 @@ UserInput.propTypes = {
   value: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  onEnterPress: PropTypes.func.isRequired, 
 };
 
 export default UserInput;
--- src/pages/loginpage/login.jsx
+++ src/pages/loginpage/login.jsx
@@ -1,76 +1,79 @@
-import React, { useState } from 'react' // eslint-disable-line no-unused-vars
+// Login.jsx
+
+import React, { useState } from 'react';
 import "./login.css";
 import { useNavigate } from 'react-router-dom';
-import UserBtn from './UserBtn.jsx'
-import UserInput from './UserInput.jsx'
+import UserBtn from './UserBtn.jsx';
+import UserInput from './UserInput.jsx';
 import Navbar from '../navbar/navbar.jsx';
 
 const Login = () => {
-  //유저 id, password
   const [userInfo, setUserInfo] = useState({
     username: '',
     password: '',
   });
- 
-  //아이디, 비밀번호
+
+  const navigate = useNavigate();
+
   const handleInputChange = event => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
     setUserInfo(userInfo => ({
       ...userInfo,
       [name]: value,
     }));
   };
 
-  //페이지 이동
-  const navigate = useNavigate();
-
-  //회원가입 페이지 이동
   const goSignupPage = () => {
     navigate('/signup');
   };
 
-  //로그인 버튼 클릭시 메인 페이지로 이동
   const loginProgcess = () => {
-  fetch('http://3.39.228.42/users/login/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify({
-      username: userInfo.username,
-      password: userInfo.password,
-    }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.token)
-      if (data.token) {
-        alert('로그인 되었습니다.');
-        localStorage.setItem('token', data.token);
-        navigate('/');
-      } else {
-        console.log('username:' ,userInfo.username, 'password:', userInfo.password);
-        alert('가입되지 않은 정보입니다.');
-      }
-    });
+    fetch('http://3.39.228.42/users/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        username: userInfo.username,
+        password: userInfo.password,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.token) {
+          alert('로그인 되었습니다.');
+          localStorage.setItem('token', data.token);
+          navigate('/');
+        } else {
+          alert('가입되지 않은 정보입니다.');
+        }
+      });
+  };
+
+  const handleEnterPress = () => {
+    loginProgcess(); // 엔터 키를 누를 때 로그인 처리 함수 호출
   };
 
   return (
     <>
       <Navbar />
       <div className="login">
-        <div className="userFrame" onChange={handleInputChange}>
+        <div className="userFrame">
           <UserInput
             type="text"
             placeholder="아이디를 입력하세요."
             value={userInfo.username}
             name="username"
+            onChange={handleInputChange}
+            onEnterPress={handleEnterPress} // 엔터 키 처리 함수 전달
           />
           <UserInput
             type="password"
             placeholder="비밀번호를 입력하세요."
             value={userInfo.password}
             name="password"
+            onChange={handleInputChange}
+            onEnterPress={handleEnterPress} // 엔터 키 처리 함수 전달
           />
           <div className='Wrapper'>
             <UserBtn
@@ -85,6 +88,6 @@ const Login = () => {
       </div>
     </>
   );
-}
+};
 
-export default Login
+export default Login;
