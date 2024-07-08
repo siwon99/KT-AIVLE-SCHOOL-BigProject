ecc0a3fe5dc6d7c95d6d5e7e655fb78fa233ab23
siwon99 <kimsiwon0707@gmail.com>
2024-07-08 11:40:51 +0900

    feat: 지도에 위치 띄우기

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import DetailPage from './pages/detailpage/detail';
 import Signup from './pages/signuppage/signup';
 import FaqPage from './pages/faqpage/faq';
 import Logout from './pages/loginpage/logout';
+import Map from './pages/map/map'
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/detail/:farm_id" element={<DetailPage />} />
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </Router>
   );
--- src/pages/map/map.jsx
+++ src/pages/map/map.jsx
@@ -1,12 +1,40 @@
-import React from "react"; // eslint-disable-line no-unused-vars
+import React, { useEffect } from 'react'; // eslint-disable-line no-unused-vars
+import './map.css';
 
-const Map = () => {
+const { kakao } = window;
 
-    return (
-        <>
-            <div>지도</div>
-        </>
-    )
+function Map() {
+  useEffect(() => {
+    if (typeof window.kakao !== "undefined") {
+      const position = new kakao.maps.LatLng(36.345401, 127.384530);
+
+      const container = document.getElementById('map');
+      const options = {
+        center: position,
+        level: 3
+      };
+      //지도 불러오기
+      const map = new kakao.maps.Map(container, options);
+
+      //마커 생성
+      const marker = new kakao.maps.Marker({
+        position: position
+      });
+      marker.setMap(map);
+
+      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content: '<div class="memo">농지</div>'
+      });
+      infowindow.open(map, marker);
+
+    }
+  }, []);
+
+  return (
+    <div id='map' style={{ width: '800px', height: '800px' }}>
+    </div>
+  );
 }
 
-export default Map; 
+export default Map;
