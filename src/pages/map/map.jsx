26b915af1928ac1b20ba7f88d1b3ea789fbcaa93
siwon99 <kimsiwon0707@gmail.com>
2024-07-08 16:11:21 +0900

    feat: 리스트에서 위도 경도 받아와서 마커로 띄우기

--- src/pages/map/map.jsx
+++ src/pages/map/map.jsx
@@ -1,39 +1,88 @@
-import React, { useEffect } from 'react'; // eslint-disable-line no-unused-vars
+import React, { useEffect, useState } from 'react'; // eslint-disable-line no-unused-vars
+import Navbar from '../navbar/navbar';
+import { useNavigate } from 'react-router-dom';
 import './map.css';
 
 const { kakao } = window;
 
 function Map() {
-  useEffect(() => {
-    if (typeof window.kakao !== "undefined") {
-      const position = new kakao.maps.LatLng(36.345401, 127.384530);
+  const navigate = useNavigate();
+  const [farms, setFarms] = useState([]);
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if(token) {
+      fetch('http://3.39.228.42/farms/list/', { 
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        }, 
+      })
+      .then(response => {
+        console.log('response:', response);
+        
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+      })
+      .then(data => {
+        if(data && data.results) {
+          for (let i = 0; i < data.results.length; i++) {
+            console.log(`Longitude${i}:`, data.results[i].longitude);
+            console.log(`Latitude${i}:`, data.results[i].latitude);
+          }
+          setFarms(data.results);
+        }
+      })
+      .catch(error => {
+        alert('오류 발생')
+      });
+    }
+  }, [navigate]);
+
+  // farms 상태가 업데이트될 때마다 현재 상태를 출력
+  useEffect(() => {
+    console.log('farms:', farms);
+  }, [farms]);
+
+  useEffect(() => {
+    if (typeof window.kakao !== "undefined" && farms.length > 0) {
       const container = document.getElementById('map');
       const options = {
-        center: position,
+        center: new kakao.maps.LatLng(0, 0),
         level: 3
       };
       //지도 불러오기
       const map = new kakao.maps.Map(container, options);
 
       //마커 생성
-      const marker = new kakao.maps.Marker({
-        position: position
-      });
-      marker.setMap(map);
+      farms.forEach(farm => {
+        const position = new kakao.maps.LatLng(farm.latitude, farm.longitude);
+        const marker = new kakao.maps.Marker({
+          position: position
+        });
+        marker.setMap(map);
 
-      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      //장소 표시
       var infowindow = new kakao.maps.InfoWindow({
-        content: '<div class="memo">농지</div>'
+        content: `<div class="memo">${farm.farm_name}</div>`
       });
       infowindow.open(map, marker);
-
+      });
     }
-  }, []);
+  }, [farms]);
+
 
   return (
-    <div id='map' style={{ width: '800px', height: '800px' }}>
-    </div>
+    <>
+      <Navbar />
+      <div id='map' style={{ width: '800px', height: '800px' }}>
+      </div>
+    </>
   );
 }
 
