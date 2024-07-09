11b67f0959c567d7c8295ee11cb427d21a9aaa92
hyemin <hm2743@naver.com>
2024-07-09 17:49:47 +0900

    feat: 홈 반응형  수정(7)

--- src/pages/homepage/Search.jsx
+++ src/pages/homepage/Search.jsx
@@ -1,24 +1,13 @@
 import React, { useState } from 'react'; // eslint-disable-line no-unused-vars
 import "./Search.css"; // 권혜민 추가
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  return (
-    <div className="search-section">
-      <input
-        type="text"
-        placeholder="검색어를 입력해 주세요."
-        value={searchTerm}
-        onChange={handleChange}
-      />
-      <div className="search-icon"></div>
-    </div>
-  );
-};
+const Search = () => (
+  <div className="circle-container">
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+  </div>
+);
 
 export default Search;
