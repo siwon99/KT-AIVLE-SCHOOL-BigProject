8598e8660bf2649066ab786e1b4ae9b9a5c0455a
KOR-GEON-WOO <nextstep9902@naver.com>
2024-07-12 17:07:59 +0900

    feat: 챗봇 기관 연계 추가

--- src/pages/homepage/ChatBot.jsx
+++ src/pages/homepage/ChatBot.jsx
@@ -49,8 +49,34 @@ const ChatBot = ({ closeModal }) => {
       // 서버로부터 받은 응답 데이터 처리
       let result = data.result;
       if (result === 'irrelevant') {
-        result = '답변할 수 없는 질문입니다.';
-        // TODO: 답변할 수 없는 질문을 받았을 때 추가적인 처리
+        result = (
+          <table>
+            <thead>
+              <tr>
+                <th>기관</th>
+                <th>내용</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>농림축산식품부</td>
+                <td>농림수산정책, 귀농정책</td>
+              </tr>
+              <tr>
+                <td>농촌진흥청</td>
+                <td>품목별 농업기술정보 및 지역별 농업정보</td>
+              </tr>
+              <tr>
+                <td>농림수산식품교육문화정보원</td>
+                <td>농산물 품목별 종합정보, 농업인 정보화교육, 농어업정보화지원사업, 농산물안전정보</td>
+              </tr>
+              <tr>
+                <td>귀농·귀촌종합센터</td>
+                <td>귀농·귀촌 희망자에게 정보 탐색부터 정착 단계까지 필요한 정보 종합적 제공</td>
+              </tr>
+            </tbody>
+          </table>
+        );
       }
       // 서버의 응답 메시지를 메시지 목록에 추가
       setChatHistory(prevMessages => [...prevMessages, { text: result, sender: "bot" }]);
