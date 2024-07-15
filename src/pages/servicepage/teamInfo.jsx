1e956d762d6532d4ddf44f2c0cb196088443bdcf
siwon99 <kimsiwon0707@gmail.com>
2024-07-15 14:38:41 +0900

    fix: 이미지 최적화

--- src/pages/servicepage/teamInfo.jsx
+++ src/pages/servicepage/teamInfo.jsx
@@ -3,15 +3,15 @@ import Navbar from '../navbar/navbar';
 import './teamInfo.css';
 
 const teamMembers = [
-  { name: '강병관', role: 'BE', image: '/src/assets/강병관.png' },
-  { name: '권혜민', role: 'FE', image: '/src/assets/권혜민.png' },
-  { name: '김시원', role: 'FE', image: '/src/assets/김시원.png' },
-  { name: '이건우', role: 'FE', image: '/src/assets/이건우.png' },
-  { name: '김찬희', role: 'BE', image: '/src/assets/김찬희.png' },
-  { name: '송유경', role: 'BE', image: '/src/assets/송유경.png' },
-  { name: '문주원', role: 'AI', image: '/src/assets/문주원.png' },
-  { name: '진희원', role: 'AI', image: '/src/assets/진희원.png' },
-  { name: '한덕구', role: 'AI', image: '/src/assets/한덕구.png' },
+  { name: '강병관', role: 'BE', image: '/src/assets/강병관.webp' },
+  { name: '권혜민', role: 'FE', image: '/src/assets/권혜민.webp' },
+  { name: '김시원', role: 'FE', image: '/src/assets/김시원.webp' },
+  { name: '이건우', role: 'FE', image: '/src/assets/이건우.webp' },
+  { name: '김찬희', role: 'BE', image: '/src/assets/김찬희.webp' },
+  { name: '송유경', role: 'BE', image: '/src/assets/송유경.webp' },
+  { name: '문주원', role: 'AI', image: '/src/assets/문주원.webp' },
+  { name: '진희원', role: 'AI', image: '/src/assets/진희원.webp' },
+  { name: '한덕구', role: 'AI', image: '/src/assets/한덕구.webp' },
 ];
 
 const sortedMembers = teamMembers.sort((a, b) => a.name.localeCompare(b.name));
