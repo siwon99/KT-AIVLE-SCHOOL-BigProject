014b169b693da2830917d5a236a460e11fa1e64a
siwon99 <kimsiwon0707@gmail.com>
2024-07-15 15:43:58 +0900

    feat: admin 추가

---
+++ src/pages/detailpage/detail_admin.jsx
@@ -0,0 +1,160 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Navbar from '../navbar/navbar';
+import './detail.css';
+
+// 날짜함수
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}년 ${month}월 ${day}일`;
+};
+
+// 시간함수
+const formatTime = (timeString) => {
+  const time = new Date(`1970-01-01T${timeString}Z`); 
+  let hours = time.getHours();
+  const minutes = time.getMinutes();
+  const period = hours >= 12 ? '오후' : '오전';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  const formattedTime = `${period} ${hours}:${minutes.toString().padStart(2, '0')}`;
+  return formattedTime;
+};
+
+// 상세 정보 페이지
+const DetailPage = () => {
+  const navigate = useNavigate();
+  const { farm_id } = useParams();
+
+  const [landDetail, setLandDetail] = useState({
+    farm_id: 0,
+    farm_name: '',
+    farm_owner: '',
+    farm_size: 0,
+    latitude: 0,
+    longitude: 0,
+    image: null,
+  });
+
+  const [landLog, setLandLog] = useState({
+    farm_status: 0,
+    farm_created: '',
+    user_id: 0
+  });
+
+  //농지 상세정보 변수
+  const [farmDate, setFarmDate] = useState('');
+  const [farmTime, setFarmTime] = useState('');
+  const [farmStatusText, setFarmStatusText] = useState('');
+
+  useEffect(() => {
+    if (!farm_id) return;
+    // 사용자가 로그인할 때 저장된 토큰
+    const token = localStorage.getItem('token');
+    if (token) {
+      fetch(`http://3.39.228.42/farms/admin/detail/${farm_id}`, { 
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        }, 
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+      })
+      .then(data => {
+        console.log('data:', data)
+        // console.log('img', data.image.farm_image)
+
+        setLandDetail({
+          farm_id: data.farm_id,
+          farm_name: data.farm_name,
+          farm_owner: data.farm_owner,
+          farm_size: data.farm_size,
+          latitude: data.latitude,
+          longitude: data.longitude,
+          image: data.image ? { farm_image: data.image.farm_image } : null
+        })
+
+        //날짜 및 시간
+        setLandLog(data.status_logs[0]);
+        setFarmDate(formatDate(data.status_logs[0].farm_created));
+        setFarmTime(formatTime(data.status_logs[0].farm_created.split('T')[1].split('Z')[0]));
+
+        //분양 상태 설정
+        if (data.status_logs.length > 0) {
+          const lastLog = data.status_logs[data.status_logs.length - 1];
+          
+          if (lastLog.farm_status === 0) {
+            setFarmStatusText('분양 완료');
+          } else if (lastLog.farm_status === 1) {
+            setFarmStatusText('유휴 농지');
+          } else if (lastLog.farm_status === 2) {
+            setFarmStatusText('불법 건축물');
+          } else {
+            setFarmStatusText('알 수 없음');
+          }
+        }
+      })
+      // 로컬 스토리지에만 토큰이 남아있어도 로그인 페이지로 유도 가능
+      .catch(error => {
+        navigate('/notfound');
+      });
+    } else {
+      navigate('/login');
+    }
+  }, [farm_id, navigate]);
+
+  const handleBackClick =() => {
+    localStorage.setItem('currentFarmId', farm_id);
+    navigate('/land_list');
+  }
+
+  const handleRentClick = () => {
+    alert('임대 신청이 완료되었습니다.');
+    localStorage.setItem('currentFarmId', farm_id);
+    navigate('/land_list');
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="detail-info">
+          <div className="d-title">농지 상세정보</div>
+          <div className="details">
+            <span className="d-info"><div className='info-title'>농지명:</div> {landDetail.farm_name}</span> 
+            <span className="d-info"><div className='info-title'>소유주:</div> {landDetail.farm_owner}</span> 
+            <span className="d-info"><div className='info-title'>위도 / 경도:</div> {landDetail.latitude}, {landDetail.longitude}</span> 
+            <span className="d-info"><div className='info-title'>농지 크기:</div> {landDetail.farm_size}</span> 
+            <span className="d-info"><div className='info-title'>게시 날짜:</div> {farmDate}</span>
+            <span className="d-info"><div className='info-title'>게시 시간:</div> {farmTime}</span>
+            <span className="d-info"><div className='info-title'>농지 상태:</div> {farmStatusText}</span>
+            <div className="btn">
+              <button onClick={handleBackClick} className="backBtn">이전</button>
+              <button onClick={handleRentClick} className="rentBtn">임대</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="map">
+          {landDetail.image ? (
+            <img src={landDetail.image.farm_image} alt="FarmImg" />
+          ) : (
+            <p className='noImg'>이미지가 등록되지 않았습니다.</p>
+          )}
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default DetailPage;
---
+++ src/pages/listpage/listpage_admin.jsx
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../navbar/navbar';
+import './listpage.css';
+
+// 상세 정보 페이지 컴포넌트
+const ListPage = () => {
+  const navigate = useNavigate();
+  const [farms, setFarms] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 5; // 한 페이지에 나열할 목록 수
+  const pageCount = 5; // 표시할 페이지 번호 갯수
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    
+    if (token) {
+      fetch('http://3.39.228.42/farms/admin/list/', { 
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        }, 
+      })
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+      })
+      .then(data => {
+        console.log('data:', data);
+        setFarms(data.reverse());
+
+        //currentFarmId에 맞는 페이지 설정
+        const currentFarmId = localStorage.getItem('currentFarmId');
+        if (currentFarmId) {
+          const index = data.findIndex(farm => farm.farm_id.toString() === currentFarmId);
+          if (index !== -1) {
+            const page = Math.ceil((index + 1) / pageSize);
+            setCurrentPage(page);
+            localStorage.removeItem('currentFarmId');
+          }
+        }
+
+      })
+      // 로컬 스토리지에만 토큰이 남아있어도 로그인 페이지로 유도 가능
+      .catch(error => {
+        navigate('/notfound');
+      });
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+  // farms 상태가 업데이트될 때마다 현재 상태를 출력
+  useEffect(() => {
+    console.log('farms:', farms);
+  }, [farms]);
+
+  // 현재 페이지에 맞는 농지 리스트를 반환
+  const lastIndex = currentPage * pageSize;
+  const firstIndex = lastIndex - pageSize;
+  const currentItems = farms.slice(firstIndex, lastIndex);
+
+  // 총 페이지 수 계산
+  const totalPages = Math.ceil(farms.length / pageSize);
+
+  // 표시할 페이지 범위 계산
+  const startPage = Math.floor((currentPage - 1) / pageCount) * pageCount + 1;
+  const endPage = Math.min(startPage + pageCount - 1, totalPages);
+
+  // 페이지 변경 핸들러
+  const changePage = (newPage) => {
+    if (newPage > 0 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
+  // 페이지네이션 버튼 렌더링 함수
+  const Pagination = (page, symbol, isDisabled) => (
+    <button onClick={() => changePage(page)} disabled={isDisabled}>
+      {symbol}
+    </button>
+  );
+
+  // 농지 선택 시 해당 farm_id localStorage에 저장
+  const handleFarmDetail = (farmId) => {
+    localStorage.setItem('selectedFarmId', farmId);
+    navigate(`/detail/${farmId}`);
+  };
+
+  return (
+    <div className="page">
+      <Navbar />
+      <div className='listpage'>
+        <div className="lists-container">
+          <div className="title">농지 리스트</div>
+
+          <div className='lists-info'>
+            {currentItems.length > 0 ? (
+              currentItems.map((farm, index) => (
+                <div className='lists' key={index}>
+                  <div className='farm-info'>
+                    <div className='num'>{farms.length - (firstIndex + index)}.</div>
+                    <div className='farm-name'>[{farm.farm_name}]</div>
+                    <div className='farm-owner'>소유자: {farm.farm_owner}</div>
+                    <div className='farm-size'>농지 크기: {farm.farm_size}</div>
+
+                    <button onClick={() => handleFarmDetail(farm.farm_id)} className="backBtn">선택</button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="no-farms">게시된 농지가 없습니다.</p>
+            )}
+          </div>
+
+          <div className='pagination-container'>
+            <div className="pagination">
+              {Pagination(1, "<<", currentPage === 1)}
+              {Pagination(currentPage - 1, "<", currentPage === 1)}
+              {Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i).map(number => (
+                <button 
+                  key={number} 
+                  onClick={() => changePage(number)} 
+                  className={number === currentPage ? 'active' : ''}
+                >
+                  {number}
+                </button>
+              ))}
+              {Pagination(currentPage + 1, ">", currentPage === totalPages)}
+              {Pagination(totalPages, ">>", currentPage === totalPages)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListPage;
