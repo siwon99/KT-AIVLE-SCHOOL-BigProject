ca2ba62eda6abf7556f753750aa976f82ab4655a
siwon99 <kimsiwon0707@gmail.com>
2024-07-15 13:47:12 +0900

    fix: React.lazy 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'; 
-import React, { useEffect, useState } from 'react'; // eslint-disable-line no-unused-vars
+import './App.css';
+import React, { useEffect, useState, lazy, Suspense } from 'react'; // eslint-disable-line no-unused-vars
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/homepage/home';
-import Login from './pages/loginpage/login';
-import ListPage from './pages/listpage/listpage';
-import ServicePage from './pages/servicepage/service';
-import DetailPage from './pages/detailpage/detail';
-import Signup from './pages/signuppage/signup';
-import FaqPage from './pages/faqpage/faq';
-import Logout from './pages/loginpage/logout';
-import Map from './pages/map/map'
-import TeamInfo from './pages/servicepage/teamInfo';
-import ListAll from './pages/listpage/listall';
-import Iblist from './pages/listpage/iblist';
-import IbDetailPage from './pages/ibdetailpage/ibdetailpage'
+
+// Lazy loading을 위해 동적으로 import
+const Home = lazy(() => import('./pages/homepage/home'));
+const Login = lazy(() => import('./pages/loginpage/login'));
+const ListPage = lazy(() => import('./pages/listpage/listpage'));
+const ServicePage = lazy(() => import('./pages/servicepage/service'));
+const DetailPage = lazy(() => import('./pages/detailpage/detail'));
+const Signup = lazy(() => import('./pages/signuppage/signup'));
+const FaqPage = lazy(() => import('./pages/faqpage/faq'));
+const Logout = lazy(() => import('./pages/loginpage/logout'));
+const Map = lazy(() => import('./pages/map/map'));
+const TeamInfo = lazy(() => import('./pages/servicepage/teamInfo'));
+const ListAll = lazy(() => import('./pages/listpage/listall'));
+const Iblist = lazy(() => import('./pages/listpage/iblist'));
+const IbDetailPage = lazy(() => import('./pages/ibdetailpage/ibdetailpage'));
 
 function App() {
   const [isAdmin, setIsAdmin] = useState(false);
@@ -24,21 +26,23 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/listall" element={<ListAll />} />
-        <Route path="/land_list" element={<ListPage />} />
-        <Route path="/service" element={<ServicePage />} />
-        <Route path="/detail/:farm_id" element={<DetailPage />} />
-        <Route path="/iblist" element={<Iblist />} />
-        <Route path="/ibdetail/:farm_id" element={<IbDetailPage />} />
-        <Route path="/faq" element={<FaqPage />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/teaminfo" element={<TeamInfo />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/listall" element={<ListAll />} />
+          <Route path="/land_list" element={<ListPage />} />
+          <Route path="/service" element={<ServicePage />} />
+          <Route path="/detail/:farm_id" element={<DetailPage />} />
+          <Route path="/iblist" element={<Iblist />} />
+          <Route path="/ibdetail/:farm_id" element={<IbDetailPage />} />
+          <Route path="/faq" element={<FaqPage />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/teaminfo" element={<TeamInfo />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
