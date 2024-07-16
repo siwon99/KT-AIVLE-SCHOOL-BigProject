65df5ced857df4337ffbdc8e8029598d854c0e8a
byeonggwan <choosymail@gmail.com>
2024-07-16 17:21:46 +0900

    feat: mypage 로그 명세

--- src/pages/mypage/usermypage.jsx
+++ src/pages/mypage/usermypage.jsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../navbar/navbar.jsx';
 import { getUserMypage } from '../../service/apiService.js';
+import './usermypage.css'
 
 const UserMyPage = () => {
 	const navigate = useNavigate();
+	const [logs, setLogs] = useState([]);
 
 	useEffect(() => {
 		const token = localStorage.getItem('token');
 		
 		if (token) {
 			getUserMypage(token)
-			.then(logs => {
-				console.log(logs);
+			.then(data => {
+				setLogs(data.reverse());
 			})
 			.catch(error => {
         navigate('/notfound');
@@ -25,6 +27,18 @@ const UserMyPage = () => {
 	return (
 		<>
 			<Navbar />
+			<div>
+				{logs.map((log, index) => (
+					<div key={index}>
+						<div> {log.farm_created} </div>
+						<div> {log.farm_id} </div>
+						<div> {log.farm_name} </div>
+						<div> {log.farm_status} </div>
+						<div> {log.farm_status_log_id} </div>
+						<div> {log.user_id} </div>
+					</div>
+				))}
+			</div>
 		</>
 	)
 }
