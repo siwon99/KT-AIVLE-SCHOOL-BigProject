a565d09080acfc835daedc40c5573bd033ea6dc1
byeonggwan <choosymail@gmail.com>
2024-07-16 11:29:42 +0900

    feat: add redirect in listall by authrization

--- src/pages/listpage/listall.jsx
+++ src/pages/listpage/listall.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../navbar/navbar.jsx';
 import './listall.css';
 
@@ -6,12 +7,50 @@ const FARM_IMAGE = '/src/assets/농지.gif';
 const BUILDING_IMAGE = '/src/assets/빌딩.gif';
 
 const ListAll = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
+    const token = localStorage.getItem('token');
     const preloadImages = [FARM_IMAGE, BUILDING_IMAGE];
     preloadImages.forEach(image => {
       new Image().src = image;
     });
-  }, []);
+    
+    if (token) {
+      fetch('http://3.39.228.42/users/authorization/', { 
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        }, 
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+      })
+      .then(data => {
+        console.log('data:', data);
+        // 관리자가 아니라면 바로 유휴농지 페이지를 보여줌
+        if (!data.is_staff) {
+          navigate('/land_list')
+        }
+        setLoading(false);
+      })
+      .catch(error => {
+        navigate('/notfound');
+      });
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+  if (loading) {
+    return <> <Navbar /> </>
+  }
 
   return (
     <>
@@ -19,6 +58,7 @@ const ListAll = () => {
       <div className="list-container">
         <div className="listbox">
           <div className='listbox_farm' style={{ backgroundImage: `url(${FARM_IMAGE})` }}>
+            {/* TODO: listpage_admin 을 보여줘야 함 */}
             <a href="/land_list">농지 리스트</a>
           </div>
           <div className='listbox_building' style={{ backgroundImage: `url(${BUILDING_IMAGE})` }}>
