9da30cbe4cdee5c9053c6bdf68f5876230003945
siwon99 <kimsiwon0707@gmail.com>
2024-07-18 11:39:12 +0900

    fix: listall 페이지 코드 수정

--- src/pages/listpage/listall.jsx
+++ src/pages/listpage/listall.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../navbar/navbar.jsx';
 import './listall.css';
-import { getUser } from '../../service/apiService.js';
 
 const FARM_IMAGE = '/assets/농지.gif';
 const BUILDING_IMAGE = '/assets/빌딩.gif';
@@ -10,7 +9,6 @@ const BUILDING_IMAGE = '/assets/빌딩.gif';
 const ListAll = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
-  const [isadmin, setIsadmin] = useState(false);
   
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -60,7 +58,7 @@ const ListAll = () => {
       <div className="list-container">
         <div className="listbox">
           <div className='listbox_farm' style={{ backgroundImage: `url(${FARM_IMAGE})` }}>
-            <a href="/listadmin">농지 리스트(공무원)</a>
+            <a href="/listadmin">농지 리스트</a>
           </div>
           <div className='listbox_building' style={{ backgroundImage: `url(${BUILDING_IMAGE})` }}>
             <a href="/iblist">불법 건축물 리스트 →</a>
