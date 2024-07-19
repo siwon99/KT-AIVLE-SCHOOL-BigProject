1a9a326f80efeda43fd60a239988c5f0a00a40bf
hyemin <hm2743@naver.com>
2024-07-19 14:58:44 +0900

    feat: 디테일페이지 재수정

--- src/pages/detailpage/detail_admin.jsx
+++ src/pages/detailpage/detail_admin.jsx
@@ -120,53 +120,55 @@ const DetailAdmin = () => {
   }
  
   return (
-    <>
+    <div className="page">
       <Navbar />
-      <div className="container">
-        <div className="detail-info">
-          <div className="d-title">농지 상세정보</div>
-          <div className="details">
-              <span className="d-info" info-title="농지명">
-                <span>{landDetail.farm_name}</span>
-              </span>
-              <span className="d-info" info-title="소유주">
-                <span>{landDetail.farm_owner}</span>
-              </span>
-              <span className="d-info" info-title="위도 / 경도">
-                <span>
-                  {landDetail.latitude}, {landDetail.longitude}
+      <div className="detailpage">
+        <div className="detail-container">
+          <div className="detail-info">
+            <div className="detail-title">농지 상세정보</div>
+            <div className="details">
+                <span className="d-info" info-title="농지명">
+                  <span>{landDetail.farm_name}</span>
                 </span>
-              </span>
-              <span className="d-info" info-title="농지 크기">
-                <span>{landDetail.farm_size}</span>
-              </span>
-              <span className="d-info" info-title="게시 날짜">
-                <span>{farmDate}</span>
-              </span>
-              <span className="d-info" info-title="게시 시간">
-                <span>{farmTime}</span>
-              </span>
-              <span className="d-info" info-title="농지 상태">
-                <span>{farmStatusText}</span>
-              </span>
-              <div className="btn">
-                <button onClick={handleBackClick} className="backBtn">
-                  이전
-                </button>
+                <span className="d-info" info-title="소유주">
+                  <span>{landDetail.farm_owner}</span>
+                </span>
+                <span className="d-info" info-title="위도 / 경도">
+                  <span>
+                    {landDetail.latitude}, {landDetail.longitude}
+                  </span>
+                </span>
+                <span className="d-info" info-title="농지 크기">
+                  <span>{landDetail.farm_size}</span>
+                </span>
+                <span className="d-info" info-title="게시 날짜">
+                  <span>{farmDate}</span>
+                </span>
+                <span className="d-info" info-title="게시 시간">
+                  <span>{farmTime}</span>
+                </span>
+                <span className="d-info" info-title="농지 상태">
+                  <span>{farmStatusText}</span>
+                </span>
+                <div className="btn">
+                  <button onClick={handleBackClick} className="backBtn">
+                    이전
+                  </button>
+                </div>
               </div>
-            </div>
+          </div>
+  
+          <div className="map">
+            {landDetail.image ? (
+              <img src={landDetail.image.farm_image} alt="FarmImg" />
+            ) : (
+              <p className='noImg'>이미지가 등록되지 않았습니다.</p>
+            )}
+          </div>
+  
         </div>
- 
-        <div className="map">
-          {landDetail.image ? (
-            <img src={landDetail.image.farm_image} alt="FarmImg" />
-          ) : (
-            <p className='noImg'>이미지가 등록되지 않았습니다.</p>
-          )}
-        </div>
- 
       </div>
-    </>
+    </div>
   );
 };
  
