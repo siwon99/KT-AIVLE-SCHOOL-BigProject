2a65b81ad801516a2951ebf68a5c317528584a9f
siwon99 <kimsiwon0707@gmail.com>
2024-07-23 15:00:35 +0900

    fix: 관리자 마이페이지 날짜 수정

--- src/pages/mypage/adminmypage.jsx
+++ src/pages/mypage/adminmypage.jsx
@@ -122,11 +122,7 @@ const AdminMyPage = () => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    let hours = date.getHours();
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const period = hours >= 12 ? '오후' : '오전';
-    hours = hours % 12 || 12;
-    return `${year}년 ${month}월 ${day}일 ${period} ${hours}:${minutes}`;
+    return `${year}년 ${month}월 ${day}일`;
   };
 
   return (
